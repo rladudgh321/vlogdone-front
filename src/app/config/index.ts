bd11b35d65cd14cc5318938ff8e34dcb83bc26fb
kyh <kyh_0625@naver.com>
2024-12-22 02:23:51 +0900

    aws로 백엔드를 올리기 전

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,6 +1,6 @@
 export const backUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://220.90.185.248:3065'
+    ? 'http://127.0.0.1:3065'
     : 'http://127.0.0.1:3065';
 // export const backUrl =
 //   process.env.NODE_ENV === 'production'
